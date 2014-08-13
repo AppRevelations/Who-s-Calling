493b1030cfd0505ba58c300217767387d1465439
kgarg1995 <kgarg119@gmail.com>
2014-08-13 22:55:32 +0530

    added code to start and stop service

--- src/com/apprevelations/whoscallingme/MainActivity.java
+++ src/com/apprevelations/whoscallingme/MainActivity.java
@@ -3,13 +3,36 @@ package com.apprevelations.whoscallingme;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+	
+	Button start,stop;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		start= (Button) findViewById(R.id.bstart);
+		stop= (Button) findViewById(R.id.bstop);
+		start.setOnClickListener(this);	
+		stop.setOnClickListener(this);
+	}
 
-		startService(new Intent(MainActivity.this, PhoneState.class));	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId())
+		{
+		case R.id.bstart:
+			startService(new Intent(MainActivity.this, PhoneState.class));
+			break;
+		case R.id.bstop:
+			stopService(new Intent(MainActivity.this, PhoneState.class));
+			break;
+		
+		}
 	}
 }
